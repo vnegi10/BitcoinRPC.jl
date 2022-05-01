fa0dce3498c6c771c623143380580c85d7a96828
Vikas Negi <vikas.negi10@gmail.com>
2022-05-01 23:52:00 +0200

    Add test for network stats

--- test/test_analytics.jl
+++ test/test_analytics.jl
@@ -1,6 +1,6 @@
-# Run tests on helper functions
+# Run tests on functions for blockchain and network analytics
 
-@testset verbose = true "Helper functions" begin
+@testset verbose = true "Analytics functions" begin
 
     @testset "collect_block_stats" begin
 
@@ -15,4 +15,12 @@
 
     end
 
+    @testset "collect_network_stats" begin
+
+        df_stats = collect_network_stats(AUTH, 700_000, 700_100)
+        rows, cols = size(df_stats)
+        @test rows == 101 && cols == 3
+
+    end
+    
 end
