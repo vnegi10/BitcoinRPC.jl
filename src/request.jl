63e0356db4e8eefa659b8fd9b71831b5ea42772d
Vikas Negi <vikas.negi10@gmail.com>
2022-05-07 18:54:02 +0200

    Add batch support to JSPN-RPC request

--- src/request.jl
+++ src/request.jl
@@ -47,3 +47,56 @@ function post_request(auth::UserAuth, RPC_name::String; params)
 
 	return result
 end	
+
+# Add support for batch requests
+function generate_body_batch(RPC_name::String, params)
+
+	bodies = Dict{String, Any}[]
+	i = 1
+
+	for param in params		
+		body_dict = Dict("method" => RPC_name, 
+		                 "params" => [param], 
+		                 "id" => i,
+		                "jsonrpc" => "2.0")
+		push!(bodies, body_dict)
+		i += 1
+	end
+	
+	return JSON.json(bodies)
+end
+
+function post_request_batch(auth::UserAuth, RPC_name::String; params = [])
+
+	url = "http://$(auth.name):$(auth.pass)@127.0.0.1:8332"
+
+	bodies = generate_body_batch(RPC_name, params)
+	headers = ["Content-Type" => "application/json"]
+
+	response = HTTP.request(
+        "POST",
+        url,
+        headers,
+        bodies;
+        verbose = 0,
+        retries = 2
+    )
+
+	response_dicts = String(response.body) |> JSON.parse
+
+    #= Keep it like this since results can be Strings (hashes), 
+    Numbers or Dicts =#
+	results = Any[]
+
+	for response_dict in response_dicts		
+		result = response_dict["result"]
+
+		if "time" in keys(result)
+			result["time"] = unix2datetime(result["time"])
+		end	
+
+		push!(results, result)
+	end
+
+	return results
+end
